f2325621a7c3d84b51799915b265965877c5f575
fabio2007 <fabio32246>
2013-12-11 12:30:30 +0000

    Adicionado ecrã de login

--- CityRoots/src/main/java/com/uminho/uce15/cityroots/MainActivity.java
+++ CityRoots/src/main/java/com/uminho/uce15/cityroots/MainActivity.java
@@ -1,7 +1,7 @@
 package com.uminho.uce15.cityroots;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
+//import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,7 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+//import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -62,4 +62,11 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
+    public void login(View view) {
+        // Do something in response to button click
+    }
+
+    public void registar(View view) {
+        // Do something in response to button click
+    }
 }
