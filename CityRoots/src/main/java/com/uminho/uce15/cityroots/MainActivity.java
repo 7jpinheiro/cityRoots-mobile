c6b7d4eb840e2cc274c77042f3aed2c50114e86b
fabio2007 <fabio32246>
2013-12-11 15:06:38 +0000

    Algumas alterações

--- CityRoots/src/main/java/com/uminho/uce15/cityroots/MainActivity.java
+++ CityRoots/src/main/java/com/uminho/uce15/cityroots/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.login, container, false);
             return rootView;
         }
     }
