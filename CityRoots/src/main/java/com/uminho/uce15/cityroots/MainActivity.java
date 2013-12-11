4bb2ea8aa6099ac658c0bb126ede53ba3328ec53
lgomes <lmrgomes104@gmail.com>
2013-12-11 15:10:15 +0000

    Ecrã Registar (base)

--- CityRoots/src/main/java/com/uminho/uce15/cityroots/MainActivity.java
+++ CityRoots/src/main/java/com/uminho/uce15/cityroots/MainActivity.java
@@ -40,10 +40,7 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
     /**
@@ -57,8 +54,7 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
+            return inflater.inflate(R.layout.login, container, false);
         }
     }
 
