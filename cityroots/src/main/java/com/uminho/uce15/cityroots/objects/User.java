4ced799326c48d5fa545514b457fa919baafbd0d
fabio2007 <fabio32246>
2014-01-15 17:06:05 +0000

    Classes de Objectos

--- cityroots/src/main/java/com/uminho/uce15/cityroots/objects/User.java
+++ cityroots/src/main/java/com/uminho/uce15/cityroots/objects/User.java
@@ -4,14 +4,10 @@ package com.uminho.uce15.cityroots.objects;
 public class User {
     private String firstname;
     private String surname;
-    private int gender;
-    private int dateofbirth;
 
-    public User(String firstname, String surname, int gender, int dateofbirth) {
+    public User(String firstname, String surname) {
         this.firstname = firstname;
         this.surname = surname;
-        this.gender = gender;
-        this.dateofbirth = dateofbirth;
     }
 
     public String getFirstname() {
@@ -28,21 +24,5 @@ public class User {
 
     public void setSurname(String surname) {
         this.surname = surname;
-    }
-
-    public int getGender() {
-        return gender;
-    }
-
-    public void setGender(int gender) {
-        this.gender = gender;
-    }
-
-    public int getDateofbirth() {
-        return dateofbirth;
-    }
-
-    public void setDateofbirth(int dateofbirth) {
-        this.dateofbirth = dateofbirth;
     }
 }
