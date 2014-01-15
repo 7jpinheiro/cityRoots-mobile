52bf237a6c6744f20efb6bc29765ec49bc9f164d
Marco Dinis <marcoandredinis@gmail.com>
2014-01-15 15:11:41 +0000

    DataProvider

---
+++ cityroots/src/main/java/com/uminho/uce15/cityroots/DataProvider.java
@@ -0,0 +1,45 @@
+package com.uminho.uce15.cityroots;
+
+import com.uminho.uce15.cityroots.objects.*;
+
+import java.util.List;
+
+public class DataProvider {
+
+    public DataProvider() {
+
+    }
+
+    public List<Attraction> getAttractions(){
+        return null;
+    }
+
+    public List<Comment> getComments(int poi_id){
+        return null;
+    }
+
+    public List<Event> getEvents(){
+        return null;
+    }
+
+    public List<Photo> getPhotos(int poi_id){
+        return null;
+    }
+
+    public List<Poi> getPois(){
+        return null;
+    }
+
+    public List<Route> getRoutes(){
+        return null;
+    }
+
+    public List<Service> getServices(){
+        return null;
+    }
+
+    public List<User> getUsers(){
+        return null;
+    }
+
+}
